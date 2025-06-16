6ad4cdbc3fbbe3bf9732062bdc096f5be9b72061
Gilberto Pessoni <gibapessoni@gmail.com>
2025-06-16 09:30:39 -0300

    Adiciona endpoint Todos no ReembolsoController e Ep download de arquivo

---
+++ Models/Dto/ReembolsoDto.cs
@@ -0,0 +1,15 @@
+﻿using ReembolsoBAS.Models.Enums;
+
+namespace ReembolsoBAS.Models.Dto
+{
+    public record ReembolsoDto(
+    int Id,
+    string NumeroRegistro,
+    string Solicitante,          
+    DateTime DataEnvio,
+    DateTime Periodo,
+    TipoSolicitacaoEnum TipoSolicitacao,
+    string Status,
+    decimal ValorSolicitado,
+    decimal ValorReembolsado);
+}
