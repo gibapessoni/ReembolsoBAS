c50ed58cf9b823038b5774bbbb9e674f6f092b39
Gilberto Pessoni <gibapessoni@gmail.com>
2025-06-20 16:52:35 -0300

    ajuste no endpoint solicitar reembolso

---
+++ Models/Dto/ReembolsoCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ReembolsoBAS.Models.Dto;
+
+public class ReembolsoCreateRequest
+{
+    [Required] public string Matricula { get; set; } = "";
+    [Required, RegularExpression(@"^\d{4}-(0[1-9]|1[0-2])$")]
+    public string Periodo { get; set; } = "";   // YYYY-MM
+    public List<LancamentoCreateDto> Lancamentos { get; set; } = [];
+}
